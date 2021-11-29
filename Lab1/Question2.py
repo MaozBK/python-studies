64936fc708b714ea94ab859667211f711c375955
MaozBK <39181027+MaozBK@users.noreply.github.com>
2021-11-29 12:59:38 +0200

    Update Question2.py

--- Lab1/Question2.py
+++ Lab1/Question2.py
@@ -1,5 +1,9 @@
 
 # Question2
+#write a python program that gets an integer to the x variable
+# and calculate to polynome   y=x^2+x
+
+
 x = int(input("Please enter a number: "))
 y = (x ** 2) + x
 print(y)
